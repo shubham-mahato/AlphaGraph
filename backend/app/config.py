1403450b006f2de9d18d89aca7c76ca95d087b56
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-12-16 00:58:01 +0530

    feat: added a simple config file

---
+++ backend/app/config.py
@@ -0,0 +1,22 @@
+from pydantic_core import BaseSettings
+from typing import List
+
+class Settings(BaseSettings):
+  PROJECT_NAME: str = "AlphaGraph API"
+  API_V1_STR: str = "/api/v1"
+  VERSION: str = "0.1.0"
+
+  BACKEND_CROS_ORIGINS: List[str] = [
+    "https://localhost/3000",
+    "http://localhost:8000",
+  ]
+
+  NEO4J_URI: str = "bold://localhost:7687"
+  NEO4J_USER:str = "neo4j"
+  NEO4J_PASSWORD = "password"
+
+  class Config:
+    case_sensitive = True
+    env_file = ".env"
+
+settings =Settings()
