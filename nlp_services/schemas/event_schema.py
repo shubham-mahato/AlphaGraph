f6c91b7563dce422ad91aab55e3b500f3b30c192
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-12-19 22:25:35 +0530

    Added event_schema that ensure that every event that is created is of same shape.

---
+++ nlp_services/schemas/event_schema.py
@@ -0,0 +1,12 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+class EventSchema(BaseModel):
+  event_id: str
+  title: str
+  description: Optional[str] =None
+  timestamp: Optional[str] =None
+  sentiment:float
+  event_type: str
+  mentioned_entities: List[str]
+  
