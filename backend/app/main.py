89d4f140748dfa4f3eaaf7823bff7df32aa28b82
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-12-21 20:00:38 +0530

    Allowed CROS for the frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,7 +42,7 @@ def get_application()-> FastAPI:
 
   application.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials =True,
     allow_methods = ["*"],
     allow_headers =["*"]
